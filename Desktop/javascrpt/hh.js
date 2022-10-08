e8c1885fcf920f7e8d6042af0a5465061529096d
Abdullah al mamun <nazma.rahman42@gmail.com>
2022-10-08 22:55:10 +0600

    doraemon's kahani

---
+++ Desktop/javascrpt/hh.js
@@ -0,0 +1,10 @@
+let n1=8
+let n2=2
+document.getElementById("bal").textContent=n1
+document.getElementById("bal2").textContent=n2
+let su=document.getElementById("bal")
+function add()
+{
+   let result=n1+n2
+    console.log("add clicked")
+}
